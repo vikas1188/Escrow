eca0ead6d5cc0256b1c85b5534063f85a0d76e38
Vikas <vikas.singh1188@gmail.com>
2018-06-22 13:40:14 +0530

    add ERC20 and Escrow Contract

---
+++ Escrow_simple.sol
@@ -0,0 +1,145 @@
+pragma solidity 0.4.24;
+
+import "./ERC20.sol";
+
+
+
+contract Ownable {
+  address public owner;
+
+  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+  function Ownable() public {
+    owner = msg.sender;
+  }
+
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+  function transferOwnership(address newOwner) onlyOwner public {
+    require(newOwner != address(0));
+    emit OwnershipTransferred(owner, newOwner);
+    owner = newOwner;
+  }
+}
+
+
+contract Escrow is Ownable {
+    
+    
+    event TokensLocked(address indexed userAddress,uint256 value,uint256 timeStamp,string notes);
+    
+    event TokensUnlocked(address indexed userAddress,uint256 value,uint256 timeStamp,string notes);
+    
+    ERC20 tokenContract;
+    
+    
+    function setTokenContractAddress(address _tokenContractAddress) onlyOwner public {
+        
+        tokenContract = ERC20(_tokenContractAddress);
+    }
+     
+    
+    
+    //Here we will write the code to submit the tokens to the escrow contract.
+    
+    function lockTokens(uint256 _value,string _notes) public onlyOwner {
+        
+        require(_value <= tokenContract.allowance(msg.sender, address(this)));
+        require(tokenContract.transferFrom(msg.sender, address(this), _value));
+        
+        emit TokensLocked(msg.sender,_value,now,_notes);
+        
+        
+    }
+    
+       function unlockTokens(uint256 _value,string _notes) public onlyOwner {
+        
+        require(_value<=tokenContract.balanceOf(owner));
+        
+        tokenContract.transfer(owner,_value);
+        
+        emit TokensUnlocked(msg.sender,_value,now,_notes);
+        
+        
+    }
+    
+    
+pragma solidity ^0.4.21;
+
+import "browser/ERC20.sol";
+
+
+
+contract Ownable {
+  address public owner;
+
+  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+  function Ownable() public {
+    owner = msg.sender;
+  }
+
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+  function transferOwnership(address newOwner) onlyOwner public {
+    require(newOwner != address(0));
+    emit OwnershipTransferred(owner, newOwner);
+    owner = newOwner;
+  }
+}
+
+
+contract Escrow is Ownable {
+    
+    
+    event TokensLocked(address indexed userAddress,uint256 value,uint256 timeStamp,string notes);
+    
+    event TokensUnlocked(address indexed userAddress,uint256 value,uint256 timeStamp,string notes);
+    
+    ERC20 tokenContract;
+    
+    
+    function setTokenContractAddress(address _tokenContractAddress) onlyOwner public {
+        
+        tokenContract = ERC20(_tokenContractAddress);
+    }
+     
+    
+    
+    //Here we will write the code to submit the tokens to the escrow contract.
+    
+    function lockTokens(uint256 _value,string _notes) public onlyOwner {
+        
+        require(_value <= tokenContract.allowance(msg.sender, address(this)));
+        require(tokenContract.transferFrom(msg.sender, address(this), _value));
+        
+        emit TokensLocked(msg.sender,_value,now,_notes);
+        
+        
+    }
+    
+       function unlockTokens(uint256 _value,string _notes) public onlyOwner {
+        
+        require(_value<=tokenContract.balanceOf(owner));
+        
+        tokenContract.transfer(owner,_value);
+        
+        emit TokensUnlocked(msg.sender,_value,now,_notes);
+        
+        
+    }
+    
+    
+    
+    
+}
+    
+    
+}
+
